dc64c8fdbe60954af94696c555e26c327d58f325
MinThihaKhine <minthihakhine@my.jcu.edu.au>
2025-05-31 22:21:32 +0800

    Refactor temperature program to use functions

--- prac_02/temperatures.py
+++ prac_02/temperatures.py
@@ -1,27 +1,40 @@
 """
-CP1404/CP5632 - Practical
-Pseudocode for temperature conversion
-
+Temperature Conversion Program
 Min Thiha Khine (#14686570)
 """
 
+#Constant to store menu
 MENU = """C - Convert Celsius to Fahrenheit
 F - Convert Fahrenheit to Celsius
 Q - Quit"""
-print(MENU)
-choice = input(">>> ").upper()
-while choice != "Q":
-    if choice == "C":
-        celsius = float(input("Celsius: "))
-        fahrenheit = celsius * 9.0 / 5 + 32
-        print(f"Result: {fahrenheit:.2f} F")
-    elif choice == "F":
-        fahrenheit = float(input("Fahrenheit: "))
-        celsius = 5 / 9 * (fahrenheit - 32)
-        print(f"Result: {celsius:.2f} C")
-    else:
-        print("Invalid option")
+
+
+def main(): #Main function
+    """Display menu and handle temperature conversions until user quits."""
     print(MENU)
     choice = input(">>> ").upper()
-print("Thank you.")
+    while choice != "Q": # Loops as long as user does not quit (Q)
+        if choice == "C": #Convert celsius to fahrenheit
+            celsius = float(input("Celsius: "))
+            fahrenheit = convert_celsius_to_fahrenheit(celsius)
+            print(f"Result: {fahrenheit:.2f} F")
+        elif choice == "F": #Convert fahrenheit to celsius
+            fahrenheit = float(input("Fahrenheit: "))
+            celsius = convert_fahrenheit_to_celsius(fahrenheit)
+            print(f"Result: {celsius:.2f} C")
+        else: # Invalid option
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
+
+def convert_celsius_to_fahrenheit(celsius):
+    """Convert celsius to fahrenheit."""
+    return celsius * 9.0 / 5 + 32
+
+def convert_fahrenheit_to_celsius(fahrenheit):
+    """Convert fahrenheit to celsius."""
+    return 5 / 9 * (fahrenheit - 32)
+
+main() # program starts here
 
