11053021e3d2117c88d70f502c29722a71d3b013
MinThihaKhine <minthihakhine@my.jcu.edu.au>
2025-06-15 12:06:50 +0800

    Push finished lists_warmup.py

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -2,9 +2,7 @@
 Min Thiha Khine
 14686570
 """
-
 numbers = [3, 1, 4, 1, 5, 9, 2]
-
 
 # Predicted values for expressions:
 # numbers[0] = 3 (first element)
@@ -16,3 +14,21 @@ numbers = [3, 1, 4, 1, 5, 9, 2]
 # 7 in numbers = False
 # "3" in numbers = False (string "3" is not in the list, only integer 3)
 # numbers + [6, 5, 3] = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3] (concatenated lists)
+
+
+# In the same Python file, write statements to achieve the following:
+print(f"original list: {numbers} \n")
+
+# 1. Change the first element of numbers to "ten" (the string, not the number 10)
+numbers[0] = "ten"
+print(f"After changing first element to 'ten':  {numbers} \n")
+
+# 2. Change the last element of numbers to 1
+numbers[-1] = 1
+print(f"After changing last element to 1:  {numbers} \n")
+
+# 3. Print all the elements from numbers except the first two (slice)
+print(f"All elements except first two: {numbers[2:]} \n")
+
+# 4. Print whether 9 is an element of numbers
+print(f"Is 9 an element of numbers? {9 in numbers}")
