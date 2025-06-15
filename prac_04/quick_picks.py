d19b3001792d77a627879d7c6d410d02dcc0e8ae
MinThihaKhine <minthihakhine@my.jcu.edu.au>
2025-06-15 14:40:22 +0800

    Add random line generator

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,28 +1,28 @@
+"""
+
+Write a program that asks the user how many "quick picks" they wish to generate. The program then generates that many lines of output.
+Each line consists of 6 random numbers between 1 and 45.
+These values should be stored as CONSTANTS .
+• Each line (quick pick) should not contain any repeated number.
+• Each line of numbers should be displayed in sorted (ascending) order.
+• Study the formatting below so that numbers align
+
+
+"""
+
 import random
 
 MINIMUM_RANDOM_NUMBER = 1
 MAXIMUM_RANDOM_NUMBER = 45
 NUMBER_OF_RANDOM_NUMBERS = 6
 
+# number_of_picks = int(input("How many quick picks? "))
 
-def main():
-    """Ask user for number of picks and display that many rows of 6 quick random numbers that are non-repeated and sorted"""
-    number_of_picks = int(input("How many quick picks? "))
-    for i in range(number_of_picks):
-        random_numbers = generate_picks(NUMBER_OF_RANDOM_NUMBERS, MINIMUM_RANDOM_NUMBER, MAXIMUM_RANDOM_NUMBER)
-        row_string = " ".join(f"{number:>{len(str(MAXIMUM_RANDOM_NUMBER))}}" for number in sorted(random_numbers))
-        print(row_string)
+random_numbers = []
+for i in range(NUMBER_OF_RANDOM_NUMBERS):
+    random_number = random.randint(MINIMUM_RANDOM_NUMBER, MAXIMUM_RANDOM_NUMBER)
+    while random_number in random_numbers:
+        random_number = random.randint(MINIMUM_RANDOM_NUMBER, MAXIMUM_RANDOM_NUMBER)
+    random_numbers.append(random_number)
+print(random_numbers)
 
-
-def generate_picks(number, low_bound, high_bound):
-    """Generate an array of number numbers with passed in low / high boundaries"""
-    random_numbers = []
-    for i in range(number):
-        random_number = random.randint(low_bound, high_bound)
-        while random_number in random_numbers:
-            random_number = random.randint(low_bound, high_bound)
-        random_numbers.append(random_number)
-    return random_numbers
-
-
-main()
