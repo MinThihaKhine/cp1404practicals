1684f77df9eddf7cdef7003d9ff7c8d90248c799
MinThihaKhine <minthihakhine@my.jcu.edu.au>
2025-06-15 12:31:33 +0800

    Refactor get_incomes function for readability

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -6,15 +6,18 @@ Starter code for cumulative total income program
 
 def main():
     """Display income report for incomes over a given number of months."""
-    incomes = []
     number_of_months = int(input("How many months? "))
-
-    for month in range(1, number_of_months + 1):
-        income = float(input(f"Enter income for month {month}: ")) # Refactored to use f string
-        incomes.append(income)
-
+    print()
+    incomes = get_incomes(number_of_months) # Extra refactoring for clean code
     print_report(incomes, number_of_months)
 
+def get_incomes(number_of_months):
+    """Get a list of all incomes for a given number of months."""
+    incomes = []
+    for month in range(1, number_of_months + 1):
+        income = float(input(f"Enter income for month {month}: "))  # Refactored to use f string
+        incomes.append(income)
+    return incomes
 
 def print_report(incomes, number_of_months):
     print("\nIncome Report\n-------------")
@@ -24,8 +27,4 @@ def print_report(incomes, number_of_months):
         total += income
         print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
 
-
 main()
-
-
-
