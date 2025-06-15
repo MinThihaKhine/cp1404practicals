4862e958d18b048e3034010458814b17e886f0b9
MinThihaKhine <minthihakhine@my.jcu.edu.au>
2025-06-15 12:22:53 +0800

    extract print_report method

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -5,7 +5,7 @@ Starter code for cumulative total income program
 
 
 def main():
-    """Display income report for incomes over a given number of number_of_months."""
+    """Display income report for incomes over a given number of months."""
     incomes = []
     number_of_months = int(input("How many months? "))
 
@@ -13,6 +13,10 @@ def main():
         income = float(input(f"Enter income for month {month}: ")) # Refactored to use f string
         incomes.append(income)
 
+    print_report(incomes, number_of_months)
+
+
+def print_report(incomes, number_of_months):
     print("\nIncome Report\n-------------")
     total = 0
     for month in range(1, number_of_months + 1):
