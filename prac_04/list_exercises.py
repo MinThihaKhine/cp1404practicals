64342cabecd8aff60dff1a506e6c28e89436f0d1
MinThihaKhine <minthihakhine@my.jcu.edu.au>
2025-06-15 13:49:34 +0800

    push list_exercises.py

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,8 +1,12 @@
+"""list_exercises.py
+Min Thiha Khine
+#14686570
+"""
+#Constants
 NUMBER_OF_PROMPTS = 5
 USERNAMES = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
              'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer',
              'bob']  # Change CONSTENT NAME
-
 
 def main():
     numbers = []
@@ -10,12 +14,10 @@ def main():
     display_numbers_details(numbers)
     validate_username()
 
-
 def get_numbers(numbers):
     for i in range(NUMBER_OF_PROMPTS):
         number = int(input("Number: "))
         numbers.append(number)
-
 
 def display_numbers_details(numbers):
     print(f"The first number is {numbers[0]}")
@@ -24,13 +26,11 @@ def display_numbers_details(numbers):
     print(f"The largest number is {max(numbers)}")
     print(f"The average of the numbers is {sum(numbers) / len(numbers)}")
 
-
 def validate_username():
     username = input("Please enter your username:  ")
     if username in USERNAMES:
         print("Access granted")
     else:
         print("Access denied")
-
 
 main()
