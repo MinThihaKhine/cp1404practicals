c8cae1cd58fc939b3e209b235dd8b7b2b6b494cd
MinThihaKhine <minthihakhine@my.jcu.edu.au>
2025-06-15 12:01:03 +0800

    Add first part of the task

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -1,1 +1,18 @@
+""" lists_warm_up.py
+Min Thiha Khine
+14686570
+"""
+
 numbers = [3, 1, 4, 1, 5, 9, 2]
+
+
+# Predicted values for expressions:
+# numbers[0] = 3 (first element)
+# numbers[-1] = 2 (last element)
+# numbers[3] = 1 (fourth element, index 3)
+# numbers[:-1] = [3, 1, 4, 1, 5, 9] (all elements except the last)
+# numbers[3:4] = [1] (from index 3 to 4 but not including 4)
+# 5 in numbers = True
+# 7 in numbers = False
+# "3" in numbers = False (string "3" is not in the list, only integer 3)
+# numbers + [6, 5, 3] = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3] (concatenated lists)
