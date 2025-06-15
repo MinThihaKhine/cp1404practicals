2ee33eb375281281cbb79fce1e1b84be28d47680
MinThihaKhine <minthihakhine@my.jcu.edu.au>
2025-06-15 13:24:27 +0800

    Refactor usernames to USERNAMES for CONSTANT convention

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,6 +1,7 @@
 NUMBER_OF_PROMPTS = 5
-usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
-             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+USERNAMES = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer',
+             'bob']  # Change CONSTENT NAME
 
 numbers = []
 for i in range(NUMBER_OF_PROMPTS):
@@ -12,3 +13,9 @@ print(f"The last number is {numbers[-1]}")
 print(f"The smallest number is {min(numbers)}")
 print(f"The largest number is {max(numbers)}")
 print(f"The average of the numbers is {sum(numbers) / len(numbers)}")
+
+username = input("Username: ")
+if username in USERNAMES:
+    print("Access granted")
+else:
+    print("Access denied")
