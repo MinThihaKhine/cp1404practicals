8b3b6f004a060ba9ebe19a44fe6a1b822a6be88f
MinThihaKhine <minthihakhine@my.jcu.edu.au>
2025-06-15 13:23:20 +0800

    add usernames list

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,13 +1,11 @@
 NUMBER_OF_PROMPTS = 5
-
-
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
 
 numbers = []
 for i in range(NUMBER_OF_PROMPTS):
-        number = int(input("Number: "))
-        numbers.append(number)
-
-
+    number = int(input("Number: "))
+    numbers.append(number)
 
 print(f"The first number is {numbers[0]}")
 print(f"The last number is {numbers[-1]}")
