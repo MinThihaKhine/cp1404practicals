ef3c8147f7decec7ce5c04e876949ff1a0110ea2
MinThihaKhine <minthihakhine@my.jcu.edu.au>
2025-06-15 13:27:20 +0800

    refactor the entire code in main function

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -3,19 +3,22 @@ USERNAMES = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'B
              'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer',
              'bob']  # Change CONSTENT NAME
 
-numbers = []
-for i in range(NUMBER_OF_PROMPTS):
-    number = int(input("Number: "))
-    numbers.append(number)
 
-print(f"The first number is {numbers[0]}")
-print(f"The last number is {numbers[-1]}")
-print(f"The smallest number is {min(numbers)}")
-print(f"The largest number is {max(numbers)}")
-print(f"The average of the numbers is {sum(numbers) / len(numbers)}")
+def main():
+    numbers = []
+    for i in range(NUMBER_OF_PROMPTS):
+        number = int(input("Number: "))
+        numbers.append(number)
+    print(f"The first number is {numbers[0]}")
+    print(f"The last number is {numbers[-1]}")
+    print(f"The smallest number is {min(numbers)}")
+    print(f"The largest number is {max(numbers)}")
+    print(f"The average of the numbers is {sum(numbers) / len(numbers)}")
+    username = input("Please enter your username:  ")
+    if username in USERNAMES:
+        print("Access granted")
+    else:
+        print("Access denied")
 
-username = input("Please enter your username:  ")
-if username in USERNAMES:
-    print("Access granted")
-else:
-    print("Access denied")
+
+main()
