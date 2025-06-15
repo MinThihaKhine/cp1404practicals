104efab70ee63e2a567cf0e0568ec4e70ebb6831
MinThihaKhine <minthihakhine@my.jcu.edu.au>
2025-06-15 14:36:41 +0800

    Generate random number row

--- prac_04/list_comprehensions.py
+++ prac_04/list_comprehensions.py
@@ -1,52 +1,28 @@
 """
-CP1404/CP5632 Practical
-List comprehensions
-Min Thiha Khine
-#14686570
+
+Write a program that asks the user how many "quick picks" they wish to generate. The program then generates that many lines of output.
+Each line consists of 6 random numbers between 1 and 45.
+These values should be stored as CONSTANTS .
+• Each line (quick pick) should not contain any repeated number.
+• Each line of numbers should be displayed in sorted (ascending) order.
+• Study the formatting below so that numbers align
+
+
 """
 
-names = ["Bob", "Angel", "Jimi", "Alan", "Ada"]
-full_names = ["Bob Martin", "Angel Harlem", "Jimi Hendrix", "Alan Turing", "Ada Lovelace"]
+import random
 
-# for loop that creates a new list containing the first letter of each name
-first_initials = []
-for name in names:
-    first_initials.append(name[0])
-print(first_initials)
+MINIMUM_RANDOM_NUMBER = 1
+MAXIMUM_RANDOM_NUMBER = 45
+NUMBER_OF_RANDOM_NUMBERS = 6
 
-# list comprehension that does the same thing as the loop above
-first_initials = [name[0] for name in names]
-print(first_initials)
+# number_of_picks = int(input("How many quick picks? "))
 
-# list comprehension that creates a list containing the initials
-# this splits each name and adds the first letters of each part to a string
-full_initials = [name.split()[0][0] + name.split()[1][0] for name in full_names]
-print(full_initials)
+random_numbers = []
+for i in range(NUMBER_OF_RANDOM_NUMBERS):
+    random_number = random.randint(MINIMUM_RANDOM_NUMBER, MAXIMUM_RANDOM_NUMBER)
+    while random_number in random_numbers:
+        random_number = random.randint(MINIMUM_RANDOM_NUMBER, MAXIMUM_RANDOM_NUMBER)
+    random_numbers.append(random_number)
+print(random_numbers)
 
-# this example uses filtering to select only the names that start with A
-a_names = [name for name in names if name.startswith('A')]
-print(a_names)
-
-# and here's the join string method being used to create a single string from the names like:
-# 'Ada Alan Angel Bob Jimi'
-print(" ".join(sorted(names)))
-
-# TODO: list comprehension to create a list of all the full_names in lowercase format
-lowercase_full_names = [full_name.lower() for full_name in full_names]
-print(lowercase_full_names)
-
-almost_numbers = ['0', '10', '21', '3', '-7', '88', '9']
-# TODO: list comprehension to create a list of integers from the above list of strings
-numbers = [int(almost_number) for almost_number in almost_numbers]
-print(numbers)
-
-# TODO: list comprehension to create a list of only the numbers that are
-# greater than 9 from the numbers (not strings) you just created
-big_numbers = [number for number in numbers if number > 9 ]
-print(big_numbers)
-
-# TODO: (more advanced) use a list comprehension and the join string method
-# to create a string (not list) of the last names for those full names longer than 11 characters
-# the result should be: 'Harlem, Hendrix, Lovelace'
-long_last_names = ", ".join([fullname.split()[1] for fullname in full_names if len(fullname) > 11])
-print(long_last_names)
