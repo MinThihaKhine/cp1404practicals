41e60c328338c446ddb7b06af8eb4590e4ecea99
MinThihaKhine <minthihakhine@my.jcu.edu.au>
2025-06-15 14:48:08 +0800

    Push quick_picks.py

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,32 +1,30 @@
-"""
+""" Min Thiha Khine
+#14686570
 
 Write a program that asks the user how many "quick picks" they wish to generate. The program then generates that many lines of output.
 Each line consists of 6 random numbers between 1 and 45.
 These values should be stored as CONSTANTS .
 • Each line (quick pick) should not contain any repeated number.
 • Each line of numbers should be displayed in sorted (ascending) order.
-• Study the formatting below so that numbers align
+• Study the formatting below so that numbers align"""
 
 
-"""
+import random # import random module
 
-import random
-
+#CONSTANTS
 MINIMUM_RANDOM_NUMBER = 1
 MAXIMUM_RANDOM_NUMBER = 45
 NUMBER_OF_RANDOM_NUMBERS = 6
 
-number_of_picks = int(input("How many quick picks? "))
+number_of_picks = int(input("How many quick picks? ")) # Get number of picks (rows) from user
 
-
-
-def main():
+def main(): #Main Function
     for i in range(number_of_picks):
         random_numbers = generate_picks(NUMBER_OF_RANDOM_NUMBERS, MINIMUM_RANDOM_NUMBER, MAXIMUM_RANDOM_NUMBER)
-        print(random_numbers)
+        formatted_row = " ".join(f"{number:>{len(str(MAXIMUM_RANDOM_NUMBER))}}" for number in sorted(random_numbers))
+        print(formatted_row)
 
-
-def generate_picks(number, lower_bound, upper_bound ):
+def generate_picks(number, lower_bound, upper_bound ): #Generate random row function
     random_numbers = []
     for i in range(NUMBER_OF_RANDOM_NUMBERS):
         random_number = random.randint(MINIMUM_RANDOM_NUMBER, MAXIMUM_RANDOM_NUMBER)
@@ -35,8 +33,7 @@ def generate_picks(number, lower_bound, upper_bound ):
         random_numbers.append(random_number)
     return random_numbers
 
-
-main()
+main() #Code starts here
 
 
 
