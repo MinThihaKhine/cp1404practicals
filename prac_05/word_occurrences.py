fffb8a5e24021cbfcb54c7e1c0dc55de6bdc1311
MinThihaKhine <minthihakhine@my.jcu.edu.au>
2025-06-21 19:28:33 +0800

    Complete word_occurrences.py

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,7 +1,7 @@
 """
 Word Occurrences
 Estimate: 10 minutes
-Actual:      minutes
+Actual:   11 minutes
 """
 
 text_string = input("Text: ")
@@ -12,5 +12,5 @@ maximum_word_length = max(len(word) for word in words)
 for word in words:
     word_to_count[word] = word_to_count.get(word, 0) + 1
 
-for word in sorted(word_to_count):
-    print(f"{word:{maximum_word_length}} : {word_to_count[word]}")
+for word, count in sorted(word_to_count.items()):
+    print(f"{word:{maximum_word_length}} : {count}")
