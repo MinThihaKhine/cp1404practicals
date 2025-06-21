08d561884bce22768da889986a4e49f08e28e9fc
MinThihaKhine <minthihakhine@my.jcu.edu.au>
2025-06-21 19:09:15 +0800

    Implement EAFP approach for dictionary lookup

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -14,5 +14,11 @@ maximum_colour_length = max(len(colour_name) for colour_name in COLOUR_TO_HEX.ke
 for colour_name, hex_code in COLOUR_TO_HEX.items():
     print(f"{colour_name:<{maximum_colour_length}} is {hex_code}")
 
-
+colour_name = input("Enter short state: ").lower()
+while colour_name != "":
+    try:
+        print(colour_name, "is", COLOUR_TO_HEX[colour_name])
+    except KeyError:
+        print("Invalid short state")
+    hex_code = input("Enter short state: ").lower()
 
