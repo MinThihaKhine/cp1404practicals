c3edc7a29efb0a7ba260e2d27cf1fed638a01f59
MinThihaKhine <minthihakhine@my.jcu.edu.au>
2025-06-21 19:23:24 +0800

    Add word counting with dictionary .get() method

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,5 +1,15 @@
 """
 Word Occurrences
 Estimate: 10 minutes
-Actual:      minutes
+Actual:   8  minutes
 """
+
+text_string = input("Text: ")
+words = text_string.split()
+word_to_count = {}
+
+for word in words:
+    word_to_count[word] = word_to_count.get(word, 0) + 1
+
+for word in word_to_count:
+    print(f"{word} : {word_to_count[word]}")
