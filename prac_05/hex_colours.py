3ea0649243d7817739e4c2754551d9188ac26956
MinThihaKhine <minthihakhine@my.jcu.edu.au>
2025-06-21 19:02:14 +0800

    Create COLOUR_TO_HEX dictionary

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,12 @@
+""" Min Thiha Khine (#14686570)
+Hexadecimal colour lookup
+"""
+
+
+COLOUR_TO_HEX = {"aliceblue": "#f0f8ff", "cornflowerblue": "#6495ed",
+                 "hotpink": "#ff69b4", "palevioletred": "#db7093",
+                 "mediumorchid": "#ba55d3", "darkslategray": "#2f4f4f",
+                 "seagreen": "#2e8b57", "goldenrod": "#daa520",
+                 "mistyrose": "#ffe4e1", "slateblue": "#6a5acd"}
+
+
