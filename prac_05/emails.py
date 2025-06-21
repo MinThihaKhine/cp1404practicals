0b35f15572b5ef1e8488f2ba533d671daa078f2d
MinThihaKhine <minthihakhine@my.jcu.edu.au>
2025-06-21 19:39:32 +0800

    Implement main structure

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -3,3 +3,13 @@ Email
 Estimate: 15 minutes
 Actual:    minutes
 """
+
+def main():
+    """Main program"""
+    email_to_name = {}
+
+    email = input("Email: ")
+    while email != "":
+        pass
+
+main()
