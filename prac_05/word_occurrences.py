a450d0801c38eba8fc4bf927c728a2790196dc13
MinThihaKhine <minthihakhine@my.jcu.edu.au>
2025-06-21 19:26:04 +0800

    Add alphabetical sorting and column alignment

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,7 +1,7 @@
 """
 Word Occurrences
 Estimate: 10 minutes
-Actual:   8  minutes
+Actual:      minutes
 """
 
 text_string = input("Text: ")
@@ -12,5 +12,5 @@ maximum_word_length = max(len(word) for word in words)
 for word in words:
     word_to_count[word] = word_to_count.get(word, 0) + 1
 
-for word in word_to_count:
-    print(f"{word} : {word_to_count[word]}")
+for word in sorted(word_to_count):
+    print(f"{word:{maximum_word_length}} : {word_to_count[word]}")
