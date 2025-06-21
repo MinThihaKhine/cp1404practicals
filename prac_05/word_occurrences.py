377d16a0dd5d264b6d66dd766e35e83062dddee1
MinThihaKhine <minthihakhine@my.jcu.edu.au>
2025-06-21 19:23:41 +0800

    Calculate maximum word length for formatting

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -7,6 +7,7 @@ Actual:   8  minutes
 text_string = input("Text: ")
 words = text_string.split()
 word_to_count = {}
+maximum_word_length = max(len(word) for word in words)
 
 for word in words:
     word_to_count[word] = word_to_count.get(word, 0) + 1
