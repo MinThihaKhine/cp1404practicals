4b05b89f53c83e171277688fd389a92edbe7374c
MinThihaKhine <minthihakhine@my.jcu.edu.au>
2025-06-22 10:29:26 +0800

    Implement file reading and test data parsing

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -4,4 +4,27 @@ Estimate: 15 minutes
 Actual:      minutes
 """
 
+FILENAME = "wimbledon.csv"
 
+
+FILENAME = "wimbledon.csv"
+COUNTRY_INDEX = 1
+CHAMPION_INDEX = 2
+
+def main():
+    """Coordinate the reading, processing and display of Wimbledon data."""
+
+
+    records = []
+    with open(FILENAME, 'r', encoding="utf-8-sig") as in_file:
+        in_file.readline() # This is to remove the CSV header row
+        for line in in_file: # Where the actual data starts
+            parts = line.strip().split(",")
+            print(parts)
+            records.append(parts)
+    print(records)
+
+
+
+
+main()
