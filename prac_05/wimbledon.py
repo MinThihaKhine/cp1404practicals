e2555da0b38bda9ab49d27065ad5a5404aae03db
MinThihaKhine <minthihakhine@my.jcu.edu.au>
2025-06-22 10:34:09 +0800

    Refactor load_records() method

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -13,18 +13,18 @@ CHAMPION_INDEX = 2
 
 def main():
     """Coordinate the reading, processing and display of Wimbledon data."""
+    records = load_records(FILENAME)
 
 
+def load_records(filename):
+    """Read CSV file and return championship data as a list of lists."""
     records = []
-    with open(FILENAME, 'r', encoding="utf-8-sig") as in_file:
-        in_file.readline() # This is to remove the CSV header row
-        for line in in_file: # Where the actual data starts
+    with open(filename, 'r', encoding="utf-8-sig") as in_file:
+        in_file.readline()  # This is to remove the CSV header row
+        for line in in_file:  # Where the actual data starts
             parts = line.strip().split(",")
-            print(parts)
             records.append(parts)
-    print(records)
-
-
+    return records
 
 
 main()
