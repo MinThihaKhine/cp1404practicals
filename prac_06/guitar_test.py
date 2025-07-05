bbc1ff31f713043a1a8caa8df7d0243b4b1af9c4
MinThihaKhine <minthihakhine@my.jcu.edu.au>
2025-07-05 22:52:10 +0800

    create test guitar objects

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -2,3 +2,13 @@
 Estimated: 8 minutes
 Actual:  minutes
 """
+
+from prac_06.guitar import Guitar
+
+def main():
+    """Test the Guitar class methods."""
+
+    # Create test guitar objects
+    gibson = Guitar(name="Gibson L-5 CES", year=1922, cost=16035.40)
+    another_guitar = Guitar(name="Another Guitar", year=2013, cost=5000)
+
