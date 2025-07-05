d0b5390c857ce9d75af12409c37bb33e16e234ef
MinThihaKhine <minthihakhine@my.jcu.edu.au>
2025-07-05 22:30:24 +0800

    Add __init__ (constructor) for Guitar object

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -2,3 +2,11 @@
 Estimated: 15 minutes
 Actual: minutes
 """
+
+
+class Guitar:
+    def __init__(self, name="", year=0, cost=0):
+        """Initialize a Guitar instance"""
+        self.name = name
+        self.year = year
+        self.cost = cost
