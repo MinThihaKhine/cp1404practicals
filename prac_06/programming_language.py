31ebe63b261f76b142b94ae978e890032881ce99
MinThihaKhine <minthihakhine@my.jcu.edu.au>
2025-07-05 21:46:39 +0800

    Create __str__ method in programming_language.py

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -10,3 +10,8 @@ class ProgrammingLanguage:
         self.typing = typing
         self.reflection = reflection
         self.year = year
+
+    def __str__(self):
+        """Return a string representation of the ProgrammingLanguage object"""
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
+
