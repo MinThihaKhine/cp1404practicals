341176d4588c6719955c3b6d72c365dabe3cf824
MinThihaKhine <minthihakhine@my.jcu.edu.au>
2025-07-05 21:52:33 +0800

    Import and copy programming_language objects into languages.py

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -2,3 +2,10 @@
 Estimated: 8 minutes
 Actual:
 """
+
+from prac_06 import programming_language
+
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+print(python)
