423e6077dd6b9ec551adc22888c53a063e6eedc1
MinThihaKhine <minthihakhine@my.jcu.edu.au>
2025-07-05 22:33:10 +0800

    Add __str__ method in guitar.py

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -10,3 +10,10 @@ class Guitar:
         self.name = name
         self.year = year
         self.cost = cost
+
+    def __str__(self):
+        """Return a string formatting of the Guitar object"""
+        return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
+
+
+
