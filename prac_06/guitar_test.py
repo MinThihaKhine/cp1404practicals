8fb51f20f7da566e11442d56f1650d40fcf826d0
MinThihaKhine <minthihakhine@my.jcu.edu.au>
2025-07-05 23:02:55 +0800

    add run_test function to comply SRP principle

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -12,3 +12,9 @@ def main():
     gibson = Guitar(name="Gibson L-5 CES", year=1922, cost=16035.40)
     another_guitar = Guitar(name="Another Guitar", year=2013, cost=5000)
 
+def run_test(guitar_object, method_name, expected_result):
+    """Test a guitar method and display expected vs actual results."""
+    actual_result = getattr(guitar_object, method_name)()
+    print(f"{guitar_object.name} {method_name}() - Expected {expected_result}. Got {actual_result}")
+
+
