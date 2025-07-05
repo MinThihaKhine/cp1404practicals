ce9bf8412f8264605665f56847ae331c94550455
MinThihaKhine <minthihakhine@my.jcu.edu.au>
2025-07-05 22:02:34 +0800

    Loop through programming_languages list to print only dynamic languages

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -11,3 +11,11 @@ python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
 ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
 visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
 print(python)
+
+programming_languages = [python, ruby, visual_basic] # List containing the three programming languages
+
+print("The dynamically typed languages are:")
+for language in programming_languages: # Loop through all the programming languages one by one
+    if language.is_dynamic(): # If dynamic
+        print(language.name) # print the language name
+
