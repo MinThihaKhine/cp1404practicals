166f410640c01cd380aedb9d2c817ea5877b7dcc
MinThihaKhine <minthihakhine@my.jcu.edu.au>
2025-07-05 21:43:33 +0800

    Create a class constructor method for ProgrammingLanguage object

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -2,3 +2,11 @@
 Estimated: 8 minutes
 Actual:
 """
+
+class ProgrammingLanguage:
+    def __init__(self, name, typing, reflection, year):
+        """Initialize a ProgrammingLanguage object"""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
