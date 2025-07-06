eddaf50434d50ebf1d05c7783d9319f68b5dfa46
MinThihaKhine <minthihakhine@my.jcu.edu.au>
2025-07-06 10:18:50 +0800

    Implement while loop to repeatedly ask for guitar details until blank name is entered

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -2,3 +2,19 @@
 Estimated: 20 minutes
 Actual:  minutes
 """
+
+from prac_06.guitar import Guitar
+
+def main():
+    """Ask for guitar details :name, year and cost, until blank is entered then output all details"""
+    guitars = []
+    print("My guitars!")
+
+    name = input("Name: ").strip()
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: "))
+
+        guitars.append(Guitar(name, year, cost))
+        print(f"{name} ({year}) : ${cost:.2f} added.")
+        name = input("Name: ").strip()
