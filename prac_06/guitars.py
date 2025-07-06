7a878c7f873bf58711ccdc5e6ba326ac1f9e5ae1
MinThihaKhine <minthihakhine@my.jcu.edu.au>
2025-07-06 10:34:16 +0800

    Add get_valid_number function to validate proper input

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -5,6 +5,8 @@ Actual:  minutes
 
 from prac_06.guitar import Guitar
 
+MINIMUM_NUMBER = 0
+
 def main():
     """Ask for guitar details :name, year and cost, until blank is entered then output all details"""
     guitars = []
@@ -12,9 +14,28 @@ def main():
 
     name = input("Name: ").strip()
     while name != "":
-        year = int(input("Year: "))
-        cost = float(input("Cost: "))
+        year = int(get_valid_number("Year: "))
+        cost = get_valid_number("Cost: $")
 
         guitars.append(Guitar(name, year, cost))
-        print(f"{name} ({year}) : ${cost:.2f} added.")
+        print(f"{name} ({year}) : ${cost:.2f} added.\n")
         name = input("Name: ").strip()
+
+def get_valid_number(prompt):
+    """Get any valid positive number (int or float)."""
+    is_valid = False
+
+    while not is_valid:
+        try:
+            number = float(input(prompt))
+            if number > MINIMUM_NUMBER:
+                is_valid = True
+            print("Number must be positive")
+        except ValueError:
+            print("Please enter a proper number")
+    return number
+
+
+
+
+main()
