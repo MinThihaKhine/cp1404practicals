3f9ecbb9c7ca64daf26b81ee42dac53e7435b3ad
MinThihaKhine <minthihakhine@my.jcu.edu.au>
2025-07-11 22:34:22 +0800

    Update pointer_arithmetic field in language

--- prac_07/language_file_reader.py
+++ prac_07/language_file_reader.py
@@ -30,7 +30,7 @@ def main():
         pointer_arithmetic = parts[4] == "Yes"
         # Construct a ProgrammingLanguage object using the elements
         # year should be an int
-        language = ProgrammingLanguage(parts[0], parts[1], reflection, int(parts[3]))
+        language = ProgrammingLanguage(parts[0], parts[1], reflection, int(parts[3]), pointer_arithmetic)
         # Add the language we've just constructed to the list
         languages.append(language)
     # Close the file as soon as we've finished reading it
@@ -65,7 +65,7 @@ def using_namedtuple():
     file_field_names = in_file.readline().strip().split(',')
     print(file_field_names)
     # Language will be a new subclass of the tuple data type class
-    Language = namedtuple('Language', 'name, typing, reflection, year')
+    Language = namedtuple('Language', 'name, typing, reflection, year, pointer_arithmetic')
     reader = csv.reader(in_file)  # use default dialect, Excel
 
     for row in reader:
