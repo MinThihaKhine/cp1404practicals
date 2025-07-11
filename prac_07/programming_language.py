400ec5e78875f49fdc1d4660ec35c079cf36ee2d
MinThihaKhine <minthihakhine@my.jcu.edu.au>
2025-07-11 22:29:26 +0800

    Create a C++ language object in run_tests() and into the languages list because it has pointer arithmetic

--- prac_07/programming_language.py
+++ prac_07/programming_language.py
@@ -31,11 +31,12 @@ class ProgrammingLanguage:
 
 def run_tests():
     """Run simple tests/demos on ProgrammingLanguage class."""
-    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
-    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
-    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995, False)
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991, False)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991, False)
+    c_plus_plus = ProgrammingLanguage("C++", "Static", False, 1983, True)  # C++ has pointer arithmetic
 
-    languages = [ruby, python, visual_basic]
+    languages = [ruby, python, visual_basic, c_plus_plus]
     print(python)
 
     print("The dynamically typed languages are:")
