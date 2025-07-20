7c253bb80704484fd084aa8569bf27c07628d047
MinThihaKhine <minthihakhine@my.jcu.edu.au>
2025-07-20 13:35:06 +0800

    Add build method to dynamically create labels from names list

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -9,3 +9,14 @@ class DynamicLabelsApp(App):
         """Construct main app"""
         super().__init__(**kwargs)
         self.names = ["Alex", "Alice", "Wendy", "Grace"]
+
+    def build(self):
+        """ Build the Kivy app from the kv file"""
+        self.title = "Dynamic Label Generator"
+        self.root = Builder.load_file('dynamic_labels.kv')
+        for name in self.names:
+            self.root.ids.main.add_widget(Label(text=name))
+        return self.root
+
+
+DynamicLabelsApp().run()
