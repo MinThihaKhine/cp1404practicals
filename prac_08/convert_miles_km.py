9f9fc380f11c5928d8c8c3a267bd53d3b23ae879
MinThihaKhine <minthihakhine@my.jcu.edu.au>
2025-07-20 12:25:18 +0800

    Implement miles to km conversion with input validation

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -3,7 +3,7 @@ from kivy.lang import Builder
 from kivy.uix.button import Button
 from kivy.properties import StringProperty
 
-MILES_TO_KM = 1.60934
+MILES_TO_KM_RATE = 1.60934
 
 
 class MilesKMConverter(App):
@@ -13,5 +13,21 @@ class MilesKMConverter(App):
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
+
+    def get_validated_miles(self):
+        """get text input from text entry widget and convert to float, return 0 if error"""
+        try:
+            value = float(self.root.ids.input_miles.text)
+            return value
+        except ValueError:
+            return 0
+
+    def handle_conversion(self):
+        """Convert validated miles value kilometres """
+        value = self.get_validated_miles()
+        kilometres = value * MILES_TO_KM_RATE
+        self.root.ids.output_label.text = str(kilometres)
+
+
 
 MilesKMConverter().run()
