7290933e51e7194445154a3216c79e9882506945
MinThihaKhine <minthihakhine@my.jcu.edu.au>
2025-07-20 13:22:19 +0800

    Add DynamicLabelsApp class constructor with names list

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -1,3 +1,11 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.label import Label
+
+class DynamicLabelsApp(App):
+    """Creates Labels from a list of names"""
+
+    def __init__(self, **kwargs):
+        """Construct main app"""
+        super().__init__(**kwargs)
+        self.names = ["Alex", "Alice", "Wendy", "Grace"]
