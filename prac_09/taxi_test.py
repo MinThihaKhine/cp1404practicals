324505074444c173c67ba087c3d597abbb9cb62b
MinThihaKhine <minthihakhine@my.jcu.edu.au>
2025-07-26 23:11:52 +0800

    Show fare after driving taxi for 40km

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -7,3 +7,11 @@ from prac_09.taxi import Taxi
 # 1. Create a new taxi object, my_taxi, with name "Prius 1", 100 units of fuel and price of $1.23
 my_taxi = Taxi("Prius 1", 100, 1.23)
 
+# 2. Drive the taxi 40 km
+my_taxi.drive(40)
+
+# 3. Print the taxi's details and the current fare
+print(my_taxi)
+print(f"Current fare: ${my_taxi.get_fare():.2f}")
+
+
