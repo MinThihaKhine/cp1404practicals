0a2feb5e13ba28c7d96e2d35fb1e849a1857eafa
MinThihaKhine <minthihakhine@my.jcu.edu.au>
2025-07-26 23:27:04 +0800

    Push modified taxi.py and taxi_test.py

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -4,7 +4,7 @@ Taxi Test - Client code to test the Taxi class
 """
 from prac_09.taxi import Taxi
 
-# 1. Create a new taxi object, my_taxi, with name "Prius 1", 100 units of fuel and price of $1.23
+# 1. Create a new taxi object, my_taxi, with name "Prius 1", 100 units of fuel and price of $1.23.
 my_taxi = Taxi("Prius 1", 100)
 
 # 2. Drive the taxi 40 km
