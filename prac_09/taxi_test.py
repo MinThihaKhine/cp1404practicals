7c8b8a84718d6f775a417a267dabfcc3d003abe6
MinThihaKhine <minthihakhine@my.jcu.edu.au>
2025-07-26 23:10:45 +0800

    create a new taxi object in taxi_test.py

---
+++ prac_09/taxi_test.py
@@ -0,0 +1,9 @@
+""" Min Thiha Khine # 14686570
+CP1404/CP5632 Practical
+Taxi Test - Client code to test the Taxi class
+"""
+from prac_09.taxi import Taxi
+
+# 1. Create a new taxi object, my_taxi, with name "Prius 1", 100 units of fuel and price of $1.23
+my_taxi = Taxi("Prius 1", 100, 1.23)
+
