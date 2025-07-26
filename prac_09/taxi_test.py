a9e282b834eb0ec676b965162bde3d78741dd4aa
MinThihaKhine <minthihakhine@my.jcu.edu.au>
2025-07-26 23:21:32 +0800

    Set price_per_km to 1.23 as class variable for all Taxi instances

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -5,7 +5,7 @@ Taxi Test - Client code to test the Taxi class
 from prac_09.taxi import Taxi
 
 # 1. Create a new taxi object, my_taxi, with name "Prius 1", 100 units of fuel and price of $1.23
-my_taxi = Taxi("Prius 1", 100, 1.23)
+my_taxi = Taxi("Prius 1", 100)
 
 # 2. Drive the taxi 40 km
 my_taxi.drive(40)
