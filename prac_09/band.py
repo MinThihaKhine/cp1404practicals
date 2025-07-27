d24d8febd9ead4817b73bb11e69d461e441d445b
MinThihaKhine <minthihakhine@my.jcu.edu.au>
2025-07-27 14:07:23 +0800

    Add a Band class with constructor and musicians list

---
+++ prac_09/band.py
@@ -0,0 +1,8 @@
+class Band:
+    """A band contains multiple musicians."""
+
+    def __init__(self, name):
+        """Initialize a band with a name and empty list of musicians."""
+        self.name = name
+        self.musicians = []
+
