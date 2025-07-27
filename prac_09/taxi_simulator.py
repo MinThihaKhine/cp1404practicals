7e2830ad8323d79fad41e97c386471fbaa324dd3
MinThihaKhine <minthihakhine@my.jcu.edu.au>
2025-07-27 17:39:43 +0800

    import Taxi and SilverServiceTaxi for taxi_simulator.py

---
+++ prac_09/taxi_simulator.py
@@ -0,0 +1,16 @@
+""" Min Thiha Khine (#14686570)
+CP1404/CP5632 Practical
+Taxi Simulator - Step by Step Build
+"""
+from prac_09.taxi import Taxi
+from prac_09.silver_service_taxi import SilverServiceTaxi
+
+
+def main():
+    """Run the taxi simulator program."""
+
+    print("Let's drive!")
+
+
+if __name__ == '__main__':
+    main()
