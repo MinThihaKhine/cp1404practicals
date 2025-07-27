f648b3a990e37139d6169a2b4f38e5a2d9a3901f
MinThihaKhine <minthihakhine@my.jcu.edu.au>
2025-07-27 17:44:07 +0800

    Add basic menu loop structure

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -5,6 +5,7 @@ Taxi Simulator - Step by Step Build
 from prac_09.taxi import Taxi
 from prac_09.silver_service_taxi import SilverServiceTaxi
 
+MENU = "q)uit, c)hoose taxi, d)rive"
 
 def main():
     """Run the taxi simulator program."""
@@ -13,6 +14,20 @@ def main():
              SilverServiceTaxi("Hummer", 200, 4)]
 
     print("Let's drive!")
+    print(MENU)
+    choice = input(">>> ").lower()
+
+    while choice != "q":
+        if choice == "c":
+            pass
+        elif choice == "d":
+            pass
+        else:
+            print("Invalid option")
+
+        print(MENU)
+        choice = input(">>> ").lower()
+
 
 
 if __name__ == '__main__':
