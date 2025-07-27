d81d7196dc65be57626c222beda5cba12b823174
MinThihaKhine <minthihakhine@my.jcu.edu.au>
2025-07-27 17:40:36 +0800

    Add list of Taxis

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -8,6 +8,9 @@ from prac_09.silver_service_taxi import SilverServiceTaxi
 
 def main():
     """Run the taxi simulator program."""
+    taxis = [Taxi("Prius", 100),
+             SilverServiceTaxi("Limo", 100, 2),
+             SilverServiceTaxi("Hummer", 200, 4)]
 
     print("Let's drive!")
 
